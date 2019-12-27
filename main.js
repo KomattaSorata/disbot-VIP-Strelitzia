6bb3521f4b95b749a6c48f9cd97cfa4219adc92f
Sorata Fujitsuka <57311258+KomattaSorata@users.noreply.github.com>
2019-12-27 17:26:41 +0900

    Temp. remove log creation

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const fs = require('fs');
 const OpReadyTime = new Date();
 let formatted_date = OpReadyTime.getFullYear() + "-" + (OpReadyTime.getMonth() + 1) + "-" + OpReadyTime.getDate() + "_" + OpReadyTime.getHours() + ":" + OpReadyTime.getMinutes();
 const operationlog = `./log/${formatted_date}_operationlog.txt`;
-fs.writeFile(operationlog, `Log file created @ ${OpReadyTime}`, function (err) {
-      if (err) throw err;
-});
+// fs.writeFile(operationlog, `Log file created @ ${OpReadyTime}`, function (err) {
+//      if (err) throw err;
+// });
 console.log(`Log file created.`);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! | Strelitzia is up and running @ ${OpReadyTime}.`);
