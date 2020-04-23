74772113fb13ce4fb240ef50c485a025a8f5e25e
Sorata Fujitsuka <57311258+KomattaSorata@users.noreply.github.com>
2020-04-24 02:13:16 +0900

    Clearing up for complete rewrite

--- main.js
+++ main.js
@@ -1,48 +1,33 @@
-const mibunshou = require('./env_config');
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require('fs');
-const hash = require('object-hash');
+const Enmap = require("enmap");
+const fs = require("fs");
 
-// General launch seq.
-const OpReadyTime = new Date();
-let formatted_date = OpReadyTime.getFullYear() + "-" + (OpReadyTime.getMonth() + 1) + "-" + OpReadyTime.getDate() + "_" + OpReadyTime.getHours() + ":" + OpReadyTime.getMinutes();
-const operationlog = `./log/${formatted_date}_operationlog.txt`;
-// fs.writeFile(operationlog, `Log file created @ ${OpReadyTime}`, function (err) {
-//      if (err) throw err;
-// });
-console.log(`Log file created.`);
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! | Strelitzia is up and running @ ${OpReadyTime}.`);
-});
-client.once('reconnecting', () => {
-  console.log('Reconnecting!');
-});
-client.once('disconnect', () => {
-  console.log('Disconnect!');
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Main
-client.on('message', msg => {
-  if (msg.content.startsWith("!5ch") === true) {
-    if (msg.channel.type === "dm"){
-      const guild = client.guilds.get(mibunshou.serverid);
-      if (guild.member(msg.author.id)) {
-        const hashid = hash.MD5(msg.author.id);
-        var d = new Date(msg.createdTimestamp);
-        msgCreatedTime = d.getDate() + '/' + (d.getMonth()) + '/' + d.getFullYear() + " " + d.getHours() + ':' + d.getMinutes();
-        const postcontent = `**以下、名無しにかわりましてVIPがお送りします Date: ${msgCreatedTime} (GTM+9) ID:${hashid.substr(hashid.length - 6, hashid.length)}**\n${msg.content.substring(5)}`;
-        client.channels.get(mibunshou.channelid).send(postcontent);
-        msg.reply(`発送しました！ \n Your message is posted.`);
-      }else{
-        msg.reply(`発送できません！あなたは目標サーバーのメンバーではありません\nFailed to post message. You are not a member of the targetted server.`);
-      }
-    } else {
-      msg.delete();
-      msg.reply(`#5chにポストしたいの場合は私にDMしてください！\nPlease DM me is you would like to post on #5ch.`).then(msg => {msg.delete(10000)}).catch(console.error());
-    }
-  }
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
 });
 
-// Login
-client.login(mibunshou.discord_token);
+client.commands = new Enmap();
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(process.env.DISCORD_TOKEN);
