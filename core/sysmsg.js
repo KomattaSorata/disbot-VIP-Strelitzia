76e41faf1007d3f7419e7778e7d31bd06fc44462
Sorata Fujitsuka <57311258+KomattaSorata@users.noreply.github.com>
2020-04-24 23:18:09 +0900

    Progress sync

---
+++ core/sysmsg.js
@@ -0,0 +1,14 @@
+module.exports = {
+  error_message: {
+    sendInDM: "#5chにポストしたいの場合は私にDMしてください！",
+  },
+
+  success_messsage: {
+    posted: "メッセージが登録されました。",
+  },
+
+  informational_message: {
+    maintenanceAfterThreadReachLimit:
+      "当スレットがリミットコメント数に足した後、新たのスレットが有効になる前にメインテナンスに入ります。メインテナンスが終わるまで、スレットの更新や投稿ができません。",
+  },
+};
--- events/message.js
+++ events/message.js
@@ -1,6 +1,18 @@
+const sysmsg = require("../core/sysmsg");
 module.exports = (client, message) => {
   if (message.author.bot) return;
   if (message.content.indexOf(process.env.BOT_COMMAND_PREFIX) !== 0) return;
+
+  if (message.channel.type === "text") {
+    message.delete();
+    message
+      .reply(sysmsg.error_message.sendInDM)
+      .then((message) => {
+        message.delete(10000);
+      })
+      .catch(console.error());
+    return;
+  }
 
   const args = message.content
     .slice(process.env.BOT_COMMAND_PREFIX.length)
