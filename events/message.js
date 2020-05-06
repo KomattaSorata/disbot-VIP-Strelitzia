44c12689580e6a937f324cecff5dda6f4ae4e42b
Sorata Fujitsuka <57311258+KomattaSorata@users.noreply.github.com>
2020-05-06 16:54:43 +0900

    Inherited code from main repo

---
+++ events/message.js
@@ -0,0 +1,45 @@
+const sysmsg = require("../core/sysmsg");
+const User = require("../models/User");
+
+module.exports = async (client, message) => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(process.env.BOT_COMMAND_PREFIX) !== 0) return;
+
+  if (message.channel.type === "text") {
+    message.delete();
+    message
+      .reply(sysmsg.error_message.sendInDM)
+      .then((message) => {
+        message.delete(10000);
+      })
+      .catch(console.error());
+    return;
+  }
+
+  const ifUserRegistered = await User.findOne({
+    discord_id: message.author.id,
+  });
+  if (
+    message.content.startsWith(`${process.env.BOT_COMMAND_PREFIX} init`) ===
+      false &&
+    !ifUserRegistered
+  ) {
+    message.channel.send({
+      embed: {
+        color: sysmsg.color.error,
+        description: sysmsg.error_message.not_initialized,
+      },
+    });
+  }
+
+  const args = message.content
+    .slice(process.env.BOT_COMMAND_PREFIX.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const cmd = client.commands.get(command);
+
+  if (!cmd) return;
+
+  cmd.run(client, message, args);
+};
